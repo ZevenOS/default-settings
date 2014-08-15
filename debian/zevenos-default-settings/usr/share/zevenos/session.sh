519ddbc0c44dda313b90076724a856e28d267815
Leszek Lesner <leszek.lsner@web.de>
2014-08-15 16:49:44 +0200

    Initial commit

---
+++ debian/zevenos-default-settings/usr/share/zevenos/session.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Copyright © 2010 Lionel Le Folgoc <mrpouit@ubuntu.com>
+
+# Readd /usr/local/share, as startxfce4 adds it only if $XDG_DATA_DIRS is empty
+if [ -z "$XDG_DATA_DIRS" ]; then
+  XDG_DATA_DIRS="/usr/local/share"
+fi
+export XDG_DATA_DIRS="/etc/xdg/xdg-zevenos:$XDG_DATA_DIRS"
+
+exec startxfce4
